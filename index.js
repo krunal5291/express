326ba9a92e2402dd1825f44df330e4de5a012ee9
krunal5291 <krunalkalathiya11@gmail.com>
2024-05-15 10:15:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+let arr = []
+app.get('/', (req, res) => {
+    res.send(arr)
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    arr.push(req.body)
+    res.send("data: ")
+})
+
+app.delete("/:id", (req, res) => {
+    let {id} = req.params
+    arr.splice(id, 1)
+    res.send("data: ")
+})
+app.listen(8090, () => {
+    console.log("")
+})
